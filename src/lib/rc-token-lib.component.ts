2f06d242e693fb0212e15191d7d85481bfb15f50
Fatma.Bayoume <fatmaamro34@gmail.com>
2025-07-14 10:39:33 +0300

    Initial Commit of rc-token-lib

---
+++ src/lib/rc-token-lib.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-rc-token-lib',
+  imports: [],
+  template: `
+    <p>
+      rc-token-lib works!
+    </p>
+  `,
+  styles: ``
+})
+export class RcTokenLibComponent {
+
+}
---
+++ src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of rc-token-lib
+ */
+
+export * from './lib/rc-token-lib.service';
+export * from './lib/rc-token-lib.module';
